aa2bdbf99e6c4dade241e52c4e2c35d3d540db0c
distructorg35 <141553717+distructorg35@users.noreply.github.com>
2025-04-02 01:13:45 +0500

    Create auto load.js

---
+++ auto load.js
@@ -0,0 +1,12 @@
+(async function() {
+    const repo = "your-username/your-repo-name"; // Replace this!
+    const response = await fetch(`https://api.github.com/repos/${repo}/contents/articles`);
+    const files = await response.json();
+
+    files.forEach(file => {
+        if (file.name.endsWith('.html')) {
+            const title = file.name.replace('.html', '').replace(/-/g, ' ');
+            addArticle(title, "Read more...", "General", file.path);
+        }
+    });
+})();
